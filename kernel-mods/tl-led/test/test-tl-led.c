d57212a6eb2a754d702e638e825d234e82072887
esapago <santiago.pagola@ericsson.com>
2017-06-28 13:38:05 +0200

    Add test for tl-led module

---
+++ kernel-mods/tl-led/test/test-tl-led.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+
+int main(int argc, char* argv[])
+{
+	if (argc == 1)
+	{
+		fprintf(stderr, "Not enough input args\n");
+		return -1;
+	}
+
+	char *arg1 = malloc(strlen(argv[1]));
+	if (arg1)
+	{
+		if (strlen(argv[1]) != 3)
+		{
+			fprintf(stderr, "Format: LEDNR:STATE, with LEDNR={0|1|2} and STATE={0|1}\n");
+			free(arg1);
+			return -1;
+		}
+		else
+		{
+			if (argv[1][1] == ':')
+			{
+				char lednr = argv[1][0];
+				char state = argv[1][2];
+
+				if ('0' > lednr || lednr > '2')
+				{
+					fprintf(stderr, "Format: LEDNR:STATE, with LEDNR={0|1|2} and STATE={0|1}\n");
+					free(arg1);
+					return -1;
+				}
+
+				if ('0' > state || state > '1')
+				{
+					fprintf(stderr, "Format: LEDNR:STATE, with LEDNR={0|1|2} and STATE={0|1}\n");
+					free(arg1);
+					return -1;
+				}
+
+				char msg[3];
+				msg[0] = lednr;
+				msg[1] = state;
+				msg[2] = 0;
+
+				//Now open file
+				printf("Now opening /dev/tl-led, sendint STATE=%c to LED=%c ...\n", lednr, state);
+				int fd, ret;
+				fd = open("/dev/tl-led", O_RDWR);
+				if (fd < 0)
+				{
+					perror("Failed to open device");
+					free(arg1);
+					return errno;
+				}
+
+				ret = write(fd, msg, strlen(msg));
+
+				if (ret < 0)
+				{
+					perror("Error writing to device");
+					free(arg1);
+					close(fd);
+					return errno;
+				}
+
+			}
+			else
+			{
+				fprintf(stderr, "Format: LEDNR:STATE, with LEDNR={0|1|2} and STATE={0|1}\n");
+				free(arg1);
+				return -1;
+			}
+		}
+
+	}
+	return 0;
+}
